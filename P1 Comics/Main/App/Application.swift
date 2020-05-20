effe3be1b45445b86746997a2210dcc308dab406
Hoapq <pham.quang.hoa@sun-asterisk.com>
2020-05-20 14:15:46 +0700

    create HomeViewController

--- P1 Comics/Main/App/Application.swift
+++ P1 Comics/Main/App/Application.swift
@@ -19,6 +19,17 @@ class Application {
     func initApplication(with window: UIWindow?) {
         guard let window = window else { return }
         window.makeKeyAndVisible()
-        window.rootViewController = ViewController()
+        window.rootViewController = UINavigationController(rootViewController: setUpTabbar())
+    }
+    
+    private func setUpTabbar() -> UITabBarController {
+        let tabbar = UITabBarController()
+        
+        let homeVC = HomeViewController()
+        homeVC.tabBarItem = UITabBarItem(tabBarSystemItem: .mostViewed, tag: 0)
+        
+        tabbar.viewControllers = [homeVC]
+        
+        return tabbar
     }
 }
